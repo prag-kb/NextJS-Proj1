3cffd754b8ef60a398910622183556eb3c4714dc
pragkb <kbprabu6@gmail.com>
2024-06-15 23:05:49 +0530

    travel website --UI working on

---
+++ travel_website/components/Button.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface ButtonProps {
+    type: "button" | "submit"
+    label: string
+    icon?: string
+    variant: "btn_dark_green"
+}
+
+const Button = ({ type, label, icon, variant }: ButtonProps) => {
+    return (
+        <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}>
+            {icon && <Image alt='icon' src={icon} width={24} height={24} />}
+            <label className='bold-16 whitespace-nowrap'>{label}</label>
+        </button>
+    )
+}
+
+export default Button
