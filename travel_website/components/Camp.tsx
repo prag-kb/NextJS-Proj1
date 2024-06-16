42684337ade4189f2bcfa2ec2b5906f6ed24cd9b
pragkb <kbprabu6@gmail.com>
2024-06-16 13:49:44 +0530

    build fix

--- travel_website/components/Camp.tsx
+++ travel_website/components/Camp.tsx
@@ -55,7 +55,7 @@ const Camp = () => {
         <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
           <h2 className='regular-24 md:regular-32 xl:regular-64 capitalize text-white'> <strong>Feeling Lost</strong> And Not Knowing The Way?</h2>
           <p className='regular-14 xl:regular-16 mt-5 text-white'>
-          Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
+          Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That&apos;s why we are here for those of you who want to start an adventure
           </p>
 
           <Image alt='camp-2' src={"/quote.svg"} width={186} height={219} className='camp-quote' /> 
